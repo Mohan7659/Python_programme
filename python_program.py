9c61490cc58bcaacea4eec6e4af5673acbe49f41
Mohan7659 <129480563+Mohan7659@users.noreply.github.com>
2023-04-08 12:09:42 +0530

    Add files via upload

---
+++ python_program.py
@@ -0,0 +1,16 @@
+#Lets a XYZ School.
+
+#Student 100.......
+s1 ='Ramu'
+s2 ='Mahesh'
+s3 ='Satish'
+s4 ='Hareesh'
+s5 ='Naga Mohan'
+s6 ='vinay'
+s7 ='Durga'
+s8 ='Sai'
+s9 ='Hemanth'
+s10 ='Ramana'
+
+print(s1,s2,s3,s4,s5,s6,s7,s8,s9,s10)
+
